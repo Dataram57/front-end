ce25cc4aa6e78551a864ca4ae3b4443c54c8e538
Dataram57 <dataram66657@gmail.com>
2023-11-25 20:33:14 +0100

    Animation

--- index.js
+++ index.js
@@ -2,10 +2,26 @@
 //#region Slots
 
 const onSlotEnter = (event) => {
-    const slot = event.target;
+    const slot = event.target.parentNode;
+    slot.classList.add('show');
+    console.log(slot);
 };
+
 const onSlotExit = (event) => {
-    const slot = event.target;
+    const slot = event.target.parentNode;
+    slot.classList.remove('show');
+};
+
+const onSlotAnimationLoaded = (event) =>{
+    //vars
+    const img = event.target;
+    const canvas = img.parentNode.children[1];
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const ctx = canvas.getContext('2d');
+    //draw
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+    console.log(3);
 };
 
 //#endregion
@@ -43,6 +59,7 @@ window.onload = () => {
     //init vars
     const list = document.getElementsByTagName('main')[0];
     let i = 0;
+    let tag = null;
     //get pattern
     const slotPattern = ReadPattern(list);
     //write to list
@@ -59,8 +76,19 @@ window.onload = () => {
     //add events
     i = list.children.length;
     while(i-- > 0){
+        //fader
         list.children[i].children[1].addEventListener('mouseover', onSlotEnter);
         list.children[i].children[1].addEventListener('mouseleave', onSlotExit);
+        //is gif
+        if(list.children[i].children[0].src.toLowerCase().indexOf('.gif') != -1){
+            //add canvas to store image
+            tag = document.createElement('canvas');
+            list.children[i].insertBefore(tag, list.children[i].children[1]);
+            //add class
+            tag.classList.add('slot-fader');
+            //add event
+            list.children[i].children[0].addEventListener('load', onSlotAnimationLoaded);
+        }
     }
 };
 
