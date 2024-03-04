ef16c21350db768518a1446ddc6a74f2b688ea90
Dataram57 <dataram66657@gmail.com>
2024-03-04 08:30:25 +0100

    docs.ethuardo.com

--- list.js
+++ list.js
@@ -9,9 +9,9 @@ const list = //JSON couldn't be loaded through file://, so it must be loaded as 
         ,"path": "./navbar1"
         ,"thumb": "thumb.gif"
     },{
-        "name": "Navbar - Ethuardo"
-        ,"path": "./navbar1"
-        ,"thumb": "thumb.gif"
+        "name": "Ethuardo Documentation Page"
+        ,"path": "https://docs.ethuardo.com/"
+        ,"thumb": "./external_gifs/docs.ethuardo.com.gif"
     },{
         "name": "Personal Website"
         ,"path": "https://dataram57.com/"
